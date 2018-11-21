059baf2665e3a7b773be3730d5a4718fd92ac2b1
Manste <manuelle.ndamtang@yahoo.com>
2018-11-21 15:13:16 +0100

    commit manuelle classe essai, etat, partie

--- ProjetAPP/src/Mot.java
+++ ProjetAPP/src/Mot.java
@@ -1,7 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.swing.JOptionPane;
 
 public class Mot {
+	private String valeur;
 	
-	private String valeur; // valeur en String
+	public Mot(String valeur) {
+		this.valeur = valeur;
+		System.out.println(valeur);
+	}
+	
+	public Mot() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line = in.readLine();
+		//System.out.println("tu as tapé" + line);
+	}
+	
+	public int longueur() {
+		return valeur.length();
+	}
+	
+	public Mot testMot(Mot test) {
+		return new Mot("el");
+	}
+	
+	public boolean Comparer(Object objet){
+		
+		if(objet == null) {
+			return false;
+		} 
+		if(objet == this) {
+			return true;
+		}
+		Mot mot = (Mot)objet;
+		if(mot.valeur == this.valeur) {
+			return true;
+		}
+		return false;
+	}
 
 	public String getValeur() {
 		return valeur;
@@ -11,15 +49,5 @@ public class Mot {
 		this.valeur = valeur;
 	}
 	
-	public int longueur() {
-		return valeur.length();
-	}
-	
-	public boolean comparer(Mot m) {
-		if(m.valeur == this.valeur) {
-			return true;
-		}		
-		return false;
-	}
 	
 }
