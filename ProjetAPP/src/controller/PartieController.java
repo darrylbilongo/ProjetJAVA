437563bd2e25bcc9175874a4921db8eb0592a0c6
darrylbilongo <d.bilongo@students.ephec.be>
2018-11-25 20:43:39 +0100

    fix de partie

--- ProjetAPP/src/controller/PartieController.java
+++ ProjetAPP/src/controller/PartieController.java
@@ -1,5 +1,30 @@
 package controller;
 
-public class PartieController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import model.Partie;
+import view.PartieVue;
+
+public class PartieController implements ActionListener{
+	Partie model;
+	PartieVue vue;
+	
+	public PartieController(Partie model) {
+		this.model = model;
+	}
+	
+	public void initJoueur() {
+		
+	}
+	
+	public void addView(PartieVue vue) {
+		this.vue = vue;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ ProjetAPP/src/view/PartieVueConsole.java
@@ -0,0 +1,45 @@
+package view;
+
+import java.util.Observable;
+import java.util.Scanner;
+
+import controller.PartieController;
+import model.Partie;
+
+public class PartieVueConsole extends PartieVue{
+	
+	PartieVueConsole(Partie model, PartieController controller) {
+		super(model, controller);
+		// TODO Auto-generated constructor stub
+	}
+
+	private Scanner sc;
+	private String pseudoJoueur;
+	private String nomJoueur;
+	private String prenomJoueur;
+	
+	public void initConsole() {
+		System.out.println("Bonjour, \nBienvenu(e) à Motus:");
+		System.out.println("Vueillez Entrez un pseudo s'il vous plait: ");
+		sc = new Scanner(System.in);
+		pseudoJoueur = sc.next();
+		System.out.println("Vueillez Entrez votre nom s'il vous plait: ");
+		sc = new Scanner(System.in);
+		nomJoueur = sc.next();
+		System.out.println("Vueillez Entrez votre pseudo s'il vous plait: ");
+		sc = new Scanner(System.in);
+		prenomJoueur = sc.next();
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void affiche(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+}
