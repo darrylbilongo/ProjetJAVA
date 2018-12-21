fea0c9ebb3cc37d5180b959f88dfab4af55c398d
Manste <manuelle.ndamtang@yahoo.com>
2018-12-21 15:01:18 +0100

    update

--- ProjetAPP/src/test/PartieMVC.java
+++ ProjetAPP/src/test/PartieMVC.java
@@ -23,7 +23,6 @@ public class PartieMVC {
 			PartieVue console = new PartieVueConsole(model, ctrlConsole);
 			ctrlConsole.addView(console);
 		}
-		
 	}
 				
 		public static void main(String args[]) {
